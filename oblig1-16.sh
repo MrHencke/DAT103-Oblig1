d39aee41dc94c08444aa605a5baa663e425a4b93
MrHencke <hencke1@hotmail.com>
2020-08-25 15:15:54 +0200

    oblig 16

--- oblig1-16.sh
+++ oblig1-16.sh
@@ -8,4 +8,25 @@ timenow=$(date +%s)
 echo "Kjører filkontroll for $1 hvert $2 sekund"
 
 if [-f "$filename"]; then
-# Slettet fil/Endret fil
+    # Slettet fil/Endret fil
+    while sleep $secs; do
+        if [! -f "$filename" ]; then
+            echo "Filen $1 ble slettet."
+            exit
+        fi
+
+        if [ $/stat -f %m $filename -gt $timeNow]; then
+            echo "Filen $1 ble endret."
+            exit 
+        fi
+    done
+
+else
+    #Filen ble opprettet
+    while sleep $secs; do
+        if [ -f "$filename"]; then
+            echo "Filen $1 ble opprettet."
+            exit
+        fi
+    done
+fi
